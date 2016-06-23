3a1b1f2cfe2c27a3c8b3ad6fb4fb5c7bab748e6b
Timur Mingaliev <timur@mingaliev.com>
2016-06-23 23:43:27 +0300

    Create folders for tests

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import getopt
 import logging
 import os
 import shutil
@@ -13,7 +14,31 @@ logging.basicConfig(
 )
 
 
+def usage():
+        print("""
+Create test data.
+-h, --help      Show this text
+-f, --folders   Create folders sctructure for recursive folder scan tests
+""")
+
+
 def main(argv):
+    create_folders = False
+
+    # Arguments
+    try:
+        opts, args = getopt.getopt(argv, ':hf', ['folders', 'help'])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            usage()
+            sys.exit()
+        elif opt in ('-f', '--folders'):
+            create_folders = True
+
     test_image = 'tests/data/DSC_1231.JPG'
 
     if not os.path.exists(config.paths['chaos']):
@@ -33,6 +58,20 @@ def main(argv):
             logging.error(e)
 
     shutil.copy2(test_image, config.paths['chaos'])
+
+    if create_folders:
+        folders = [
+            'test',
+            'photos',
+            'photos/123',
+            'photos/june',
+            'other',
+        ]
+
+        for folder in folders:
+            folder = os.path.join(config.paths['chaos'], folder)
+            os.makedirs(folder)
+            shutil.copy2(test_image, folder)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
