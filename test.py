c25933cc36561a8670c3ab541ff7ffc1d349b2d7
Timur Mingaliev <timur@mingaliev.com>
2016-06-23 23:35:27 +0300

    Create dirs if not exists

--- test.py
+++ test.py
@@ -16,6 +16,12 @@ logging.basicConfig(
 def main(argv):
     test_image = 'tests/data/DSC_1231.JPG'
 
+    if not os.path.exists(config.paths['chaos']):
+        os.makedirs(config.paths['chaos'])
+
+    if not os.path.exists(config.paths['order']):
+        os.makedirs(config.paths['order'])
+
     for file in os.listdir(config.paths['chaos']):
         file_path = os.path.join(config.paths['chaos'], file)
         try:
